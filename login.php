90c7c3ff6a766056661f247eb8daf1ff7a887324
xp <xp@CETYSDAM00>
2019-10-30 10:31:19 +0100

    con login

--- login.php
+++ login.php
@@ -2,28 +2,19 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<!-- Title Page -->	
-	<title>Login</title>
-
-	<!-- CSS -->
-	<link rel="stylesheet" href="css/reset.css">
-	<link rel="stylesheet" href="css/animate.css">
-	<link rel="stylesheet" href="css/styles.css">	
+        <title>Login</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
 </head>
 <body>
 	<div id="container">
 		<form>
-			<!-- Username -->
-			<label for="name">Username:</label>
+			<label for="name">Nombre:</label>
 			<input type="name">
-			<!-- Password -->
-			<label for="username">Password:</label>
-			<p><a href="#">Forgot your password?</a>
+			<label for="username">Contraseña:</label>
+			<p><a href="#">¿Has olvidado tu contraseña?</a>
 			<input type="password">
 			<div id="lower">
-				<input type="checkbox"><label class="check" for="checkbox">Keep me logged in</label>
-				<!-- Submit Button -->
+				<input type="checkbox"><label class="check" for="checkbox">Guarda mis datos fiera</label>
 				<input type="submit" value="Login">
 			</div>
 		</form>		
